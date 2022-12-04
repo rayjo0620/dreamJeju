e9a9b59662b8ceef6633ac382817704e3f032591
Hururuha <Hururuha@59.15.223.19>
2022-12-05 02:29:04 +0900

    2022-12-05 rayjo0620 master . . . 최초 공유

---
+++ dreamJeju/src/main/java/com/jejupick/bl/DreamJejuApplication.java
@@ -0,0 +1,13 @@
+package com.jejupick.bl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamJejuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamJejuApplication.class, args);
+	}
+
+}
---
+++ dreamJeju/src/test/java/com/jejupick/bl/DreamJejuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jejupick.bl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamJejuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
